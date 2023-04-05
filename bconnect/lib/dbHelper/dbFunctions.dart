52286f3426991da5b5adb6bf294b9a7df2bde6e6
Anish <aniish9.18@gmail.com>
2023-04-05 17:01:40 +0530

    pushing

---
+++ bconnect/lib/dbHelper/dbFunctions.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<Map<String, List<String>>> fetchOffers() async {
+  Map<String, List<String>> offersMap = {};
+
+  try {
+    final QuerySnapshot<Map<String, dynamic>> snapshot =
+        await FirebaseFirestore.instance.collection('offers').get();
+
+    for (QueryDocumentSnapshot<Map<String, dynamic>> documentSnapshot
+        in snapshot.docs) {
+      final Map<String, dynamic> data = documentSnapshot.data();
+      final String shopName = data['shopName'];
+      final List<dynamic> offers = data['offers'];
+
+      List<String> offerList = offers.map((e) => e.toString()).toList();
+
+      offersMap[shopName] = offerList;
+    }
+  } catch (e) {
+    print('Error fetching offers data: $e');
+  }
+
+  return offersMap;
+}
