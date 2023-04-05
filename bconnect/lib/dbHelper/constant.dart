be3d7994e3a4750c24d275ae19847212b8819083
Omkar <omkarrao5123@gmail.com>
2023-04-05 14:44:13 +0530

    Resolve dependencies

---
+++ bconnect/lib/dbHelper/constant.dart
@@ -0,0 +1,6 @@
+const MONGO_CONN_URL =
+    "mongodb+srv://demo:sS6j964wI0RFH3jB@cluster0.x82htuf.mongodb.net/?retryWrites=true&w=majority";
+const USER_COLLECTION = "Locations";
+
+//const user = "demo";
+//const password = "sS6j964wI0RFH3jB";
