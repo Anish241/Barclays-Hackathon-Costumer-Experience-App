5573bd4795e9d645b1b2d756f2ab3a8d54785a07
Omkar <omkarrao5123@gmail.com>
2023-04-05 15:03:12 +0530

    Mongo Collection and Connect

--- bconnect/lib/dbHelper/constant.dart
+++ bconnect/lib/dbHelper/constant.dart
@@ -1,6 +1,6 @@
 const MONGO_CONN_URL =
-    "mongodb+srv://demo:sS6j964wI0RFH3jB@cluster0.x82htuf.mongodb.net/?retryWrites=true&w=majority";
-const USER_COLLECTION = "Locations";
+    "mongodb+srv://demo:sS6j964wI0RFH3jB@cluster0.x82htuf.mongodb.net/Barclays?retryWrites=true&w=majority";//check if you need name of database here 
+const LOCATION_COLLECTION = "Location";
 
 //const user = "demo";
 //const password = "sS6j964wI0RFH3jB";
--- bconnect/lib/dbHelper/mongodb.dart
+++ bconnect/lib/dbHelper/mongodb.dart
@@ -1,1 +1,11 @@
-class Mongo
+import 'package:bconnect/dbHelper/constant.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+class MongoDatabse{
+  static var db,locationCollection;
+  static connect() async{
+    db = await Db.create(MONGO_CONN_URL);
+    await db.open();
+    locationCollection = db.collection(LOCATION_COLLECTION);
+  }
+}
